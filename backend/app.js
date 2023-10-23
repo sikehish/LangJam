9c6c55cf0b7a5c9863f6db7eb526ff5524753360
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-24 03:14:14 +0530

    Cleaned up some code in app.js in backend dir

--- backend/app.js
+++ backend/app.js
@@ -7,22 +7,5 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-//Routes
-app.get('/', function (req, res) {
-  res.send('Simple Example of routes!');
-})
-
-app.get('/signup', function (req, res) {
-  res.send('This is demo route for sign up');
-})
-
-app.get('/signin', function (req, res) {
-  res.send('This is demo route for sign in');
-})
-
-app.get('/signin/dashboard', function (req, res) {
-  res.send('This is demo route for user who signed in and now reaches their dashboard');
-})
-
 
 module.exports=app
