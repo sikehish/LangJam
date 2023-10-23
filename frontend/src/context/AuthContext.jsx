fceaffbaf4139de434bf6b7f7dc0fd2cb2b446fb
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-23 21:00:53 +0530

    Updated AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { createContext, useReducer, useContext } from 'react';
 
-function AuthContext() {
+const AuthContext = createContext();
+
+export const useAuthContext = () => {
+  const val = useContext(AuthContext);
+  //Need to throw error if value is being acesed outside the context defined
+  return val;
+};
+
+const reducer = (state, action) => {
+  console.log("HHHHHHHHHHHHHHHHHHA")
+  if (action.type === 'SIGNUP') {
+    return { user: null, flag: 'signed up' }; //NOT IMPLEMENTING AUTO REGISTRATION ON SIGNUP
+  } else if (action.type === 'LOGIN') {
+    console.log("HAAAAAAAAAH ",action.payload)
+    return { user: action.payload };
+  } else if (action.type === 'LOGOUT') {
+    return { user: null };
+  } else return state;
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    user: JSON.parse(localStorage.getItem("user")) || null
+  });
+
   return (
-    <div>AuthContext</div>
-  )
-}
-
-export default AuthContext
+    <AuthContext.Provider value={{ dispatch, state }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
