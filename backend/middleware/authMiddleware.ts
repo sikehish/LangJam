9739cdde98d5e4a115464ae1242770712ca0112f
sikehish <hisham0502@gmail.com>
2024-03-15 06:34:26 +0530

    Modified micedAuh middleware

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -58,13 +58,17 @@ export const checkAdminAuth = (req: Request, res: Response, next: NextFunction):
 
 export const checkMixedAuth = (req: Request, res: Response, next: NextFunction): Response | void => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
-  console.log("TOKEN ",token)
   if (!token) {
     return res.status(401).json({ status: "fail", message: 'Unauthorized access. Invalid Bearer token.' });
   }
   try {
-    let decodedToken = jwt.verify(token, process.env.JWT_KEY as jwt.Secret) || jwt.verify(token, process.env.JWT_ADMIN_KEY as jwt.Secret);
-    
+    let decodedToken: any;
+
+    try {
+      decodedToken = jwt.verify(token, process.env.JWT_KEY as jwt.Secret);
+    } catch {
+      decodedToken = jwt.verify(token, process.env.JWT_ADMIN_KEY as jwt.Secret);
+    }
 
     if (!decodedToken || typeof decodedToken !== 'object' || !decodedToken.id) {
       return res.status(401).json({ status: "fail", message: "Invalid token format" });
