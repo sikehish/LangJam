8735e8be175412ec5538bdb8f131ec7202bb5150
sikehish <hisham0502@gmail.com>
2024-03-29 22:44:04 +0530

    Enhanced user schema, with attempt records included

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,22 +1,34 @@
-import { NextFunction } from "express";
-;
-import mongoose, { Document, Model, Schema } from 'mongoose';
+import mongoose, { Document, Model, Schema, Types } from 'mongoose';
 import bcrypt from 'bcrypt';
-import jwt, { Jwt } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import validator from 'validator';
 import { isEmail, isStrongPassword } from "validator"
 
-export interface UserDocument extends Document {
+interface Attempt {
+  quizId: Types.ObjectId; 
+  questionId: Types.ObjectId; 
+  chosenOption: number; 
+  isCorrect: boolean; 
+}
+
+
+interface UserDocument extends Document {
   name: string;
   email: string;
   password: string;
   isAdmin?: boolean;
   createToken(verify?: boolean): string;
+  attempts: Attempt[];
 }
 
-// export interface UserModel extends Model<UserDocument> {
-//   createToken(verify?: boolean): string;
-// }
+
+const attemptSchema = new Schema<Attempt>({
+  quizId: { type: Schema.Types.ObjectId, ref:"Quiz" ,required: true }, // Use Schema.Types.ObjectId here
+  questionId: { type: Schema.Types.ObjectId,ref:"Question", required: true }, // Use Schema.Types.ObjectId here
+  chosenOption: { type: Number, required: true },
+  isCorrect: { type: Boolean, required: true }
+});
+
 
 const userSchema = new Schema<UserDocument>({
   name: {
@@ -38,32 +50,38 @@ const userSchema = new Schema<UserDocument>({
     required: true,
     minlength: [8, 'Password must be at least 8 characters'],
     validate: [isStrongPassword, 'Password not strong enough']
-  },isAdmin:{
+  },
+  isAdmin:{
     type: Boolean,
     default: true
-  }
-  
-  //NOTE: Will be implemented later
-//   verified: {
-//     type: Boolean,
-//     required: true,
-//     default: false
-// }
+  },
+  attempts: [attemptSchema] // Array of attempts
 },{
   timestamps: true
 });
 
-//Using pre middleware function to utilise the data validation used in the schema https://mongoosejs.com/docs/validation.html
 
-userSchema.pre('save', async function (next) {
-  const user = this as UserDocument;
-  if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 10);
+// Method to record a user's attempt at answering a question
+userSchema.methods.recordAttempt = async function (
+  this: UserDocument,
+  quizId: Types.ObjectId,
+  questionId: Types.ObjectId,
+  chosenOption: number,
+  isCorrect: boolean
+) {
+  this.attempts.push({ quizId, questionId, chosenOption, isCorrect });
+  await this.save();
+};
+
+//Using pre middleware function to utilise the data validation used in the schema https://mongoosejs.com/docs/validation.html
+userSchema.pre<UserDocument>('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
   }
   next();
 });
 
-userSchema.methods.createToken = function (verify = false): string {
+userSchema.methods.createToken = function (this: UserDocument, verify = false): string {
   return verify
     ? jwt.sign({ id: this.id }, process.env.JWT_VERIFY_KEY as jwt.Secret, { expiresIn: '2d' })
     : jwt.sign({ id: this.id }, process.env.JWT_KEY as jwt.Secret, { expiresIn: '2d' });
